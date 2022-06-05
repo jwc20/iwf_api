1a519072777b5a6395285a136abb4f2b04ca66bc
jwc20 <choij2015@gmail.com>
2022-06-05 00:08:04 -0700

    added helper methods file

--- iwf/core.py
+++ iwf/core.py
@@ -58,22 +58,3 @@ ATHLETE_ID_URL = "id="
 
 
 TIME_TO_WAIT = 3
-
-
-# <---------------------------------Validations------------------------------->
-
-
-def isResult(url):
-    """
-    Validates event url
-    Example: https://iwf.sport/results/results-by-events/?event_id=529
-    """
-    return True if RESULT_URL in url else False
-
-
-def isAthleteBio(url):
-    """
-    Validate athlete url
-    Example: https://iwf.sport/weightlifting_/athletes-bios/?athlete=ilyin-ilya-1988-05-24&id=7895
-    """
-    return True if ATHLETE_BIO_URL in url else False
