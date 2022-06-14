a628f79f9126b33ea890edbc92252e99f065eaaa
jwc20 <choij2015@gmail.com>
2022-06-14 12:05:09 -0700

    using p tags to add to data

--- iwf/result.py
+++ iwf/result.py
@@ -1,5 +1,6 @@
 from .core import *
 import json
+import re
 
 # from lxml import html
 from lxml import etree
@@ -17,69 +18,145 @@ class Result(object):
     def _scrape_result_info(self, soup_data):
         result = []
 
-        data = {
-            "name": None,  # string
-            "birthdate": None,  # string
-            "nation": None,  # string
-            "athlete_url": None,  # string
-            "category": None,  # string
-            "bodyweight": None,  # string
-            "group": None,  # string
-            "snatch1": None,  # string
-            "snatch2": None,  # string
-            "snatch3": None,  # string
-            "snatch": None,  # string
-            "jerk1": None,  # string
-            "jerk2": None,  # string
-            "jerk3": None,  # string
-            "jerk": None,  # string
-            "total": None,  # string
-            "rank_sn": None,  # string
-            "rank_cj": None,  # string
-            "rank": None,  # string
-        }
+        result_container = soup_data.find_all("div", {"class": "result__container"})
+        for div_id in result_container:
 
+            # id="men_total"
+            # men_snatchjerk
+            # women_total
+            # women_snatchjerk
 
-        for div_id in soup_data.find_all("div", {"class": "result__container"}):
-            category = div_id.find_all("div", {'class': 'col-12'})
-            print(category)
-            
-                
-            for div_cl in div_id.findAll("div"):
+            # print(len(result_container))
+            # print(div_id if div_id.get("id") == "men_snatchjerk" else "ACK")
 
-                
-                name_container = div_cl.find("div", {"class": "col-7 not__cell__767"})
-                if name_container:
-                    name = name_container.find("p").string.strip()
-                    data["name"] = name
-                    data["nation"] = (
-                        div_cl.find("div", {"class": "col-3 not__cell__767"})
-                        .find("p")
-                        .text.strip()
-                    )
-                
-                
+            if (
+                div_id.get("id") == "men_snatchjerk"
+                or div_id.get("id") == "women_snatchjerk"
+            ):
+
+                category = div_id.find_all("div", {"class": "col-12"})
+
+                # data["name"] = (
+                #     div_id.find("div", {"class": "col-7 not__cell__767"})
+                #     .find("p")
+                #     .text.strip()
+                # )
+
+                count = 0
+                for item in category:
+                    h3_cat = item.find("h3")
+                    if h3_cat:
+                        # print(item)
+                        cards_container = div_id.find_all("div", {"class": "cards"})
+
+                        # getting the names, nation, and birthdate
+                        for cards in cards_container[::3]:
+
+                            # print(cards.find_all("div", {"class": "col-7 not__cell__767"}))
+                            # card_container = cards.find_all("div", {"class": "col-7 not__cell__767"})
+                            card_container = cards.find_all("div", {"class": "card"})
+                            for card in card_container[1:]:
+
+                                data = {
+                                    "name": None,  # string
+                                    "birthdate": None,  # string
+                                    "nation": None,  # string
+                                    "athlete_url": None,  # string
+                                    "category": None,  # string
+                                    "bodyweight": None,  # string
+                                    "group": None,  # string
+                                    "snatch1": None,  # string
+                                    "snatch2": None,  # string
+                                    "snatch3": None,  # string
+                                    "snatch": None,  # string
+                                    "jerk1": None,  # string
+                                    "jerk2": None,  # string
+                                    "jerk3": None,  # string
+                                    "jerk": None,  # string
+                                    "total": None,  # string
+                                    "rank_sn": None,  # string
+                                    "rank_cj": None,  # string
+                                    "rank": None,  # string
+                                }
+
+                                # print(card)
+                                # p_tags = card.find_all('p')[1]
+                                p_tags_name = card.find_all("p")[1].text.strip()
+                                # print(p_tags_name)
+                                data["name"] = p_tags_name
+                                # print(p_tags_name.text.strip())
+
+                                # print(type(p_tags))
+
+                                # for i in range(len(p_tags)):
+                                #     print(p_tags[i])
+                                result.append(data)
+                            # print(
+                            #     "#####################################end of card######################################"
+                            # )
+
+                            # print(p_tags)
+
+                            # for p_tag in p_tags:
+                            #     print("########################",p_tag)
+
+                            # name = p_tags[1]
+                            # print(name)
+                            # data["name"] =
+
+                            # print(p_tags)
+                            # for p_tag in p_tags:
+                            #     # print(p_tag.text.strip())
+                            #     print(count, p_tag)
+
+                            # count += 1
+
+                            # data["name"] = name
+                            # print(card.find_all('p')[1].text.strip())
+                            # data["nation"] = card.find()
+                        # names = div_id.find_all("div", {"class": "col-7 not__cell__767"})
+                        # for name in names:
+                        #     print(name.find('p').text.strip())
+                        # print(count, div_id.find("div", {"class": "col-7 not__cell__767"}).find("p").text.strip())
+                        # count += 1
+                        # print(count)
+                # print(div_id.find("div", {"class": "col-7 not__cell__767"}).find("p").text.strip())
+                # print(count)
+                # result.append(data)
+
+                # for div_cl in div_id.findAll("div"):
+
+                #     name_container = div_cl.find("div", {"class": "col-7 not__cell__767"})
+                #     if name_container:
+                #         name = name_container.find("p").string.strip()
+                #         data["name"] = name
+                #         data["nation"] = (
+                #             div_cl.find("div", {"class": "col-3 not__cell__767"})
+                #             .find("p")
+                #             .text.strip()
+                #         )
+
                 # if category:
                 #     data["category"] = category.text.strip()
-                    # for res_cont in div_cl.parent.find_all("h2"):
-                    #     data["category"] = res_cont.text
-                    #     print(res_cont)
-                    # print(res_cont)
-                    #     # print(sibling)
-                    #     print(res_cont.find('div', {'class': 'col-12'}))
-                    #     # category = res_cont.find('div', {'class': 'col-12'})
-                    #     # if category:
-                    #     #     print(category)
+                # for res_cont in div_cl.parent.find_all("h2"):
+                #     data["category"] = res_cont.text
+                #     print(res_cont)
+                # print(res_cont)
+                #     # print(sibling)
+                #     print(res_cont.find('div', {'class': 'col-12'}))
+                #     # category = res_cont.find('div', {'class': 'col-12'})
+                #     # if category:
+                #     #     print(category)
 
-                    # print(div_cl.parent.find_all("h2"))
+                # print(div_cl.parent.find_all("h2"))
 
-                    # result.append(data)
+                # result.append(data)
 
-                    # if div_cl.previous_sibling.find('p') == 'Snatch':
-                    #     print('ack')
-                    # print(name)
-                    # print(name.find('p').text.strip())
-                    # if name in data
+                # if div_cl.previous_sibling.find('p') == 'Snatch':
+                #     print('ack')
+                # print(name)
+                # print(name.find('p').text.strip())
+                # if name in data
 
         return result
 
