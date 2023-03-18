1d7f392837ad35df40ff0469801f7c9c8697d9bd
jwc20 <choij2015@gmail.com>
2023-03-17 18:07:07 -0700

    Update example.

--- example.py
+++ example.py
@@ -1,12 +1,11 @@
 import iwf
 
-search_keywords = []
-client = iwf.Iwf(search_keywords)
+client = iwf.Iwf()
 
-for event in client.get_events():
-  print(event)
-  print(event['name'])
+# for event in client.get_events():
+#   print(event)
+#   print(event['name'])
 
-url = "https://iwf.sport/results/results-by-events/?event_id=486"
-result = client.get_results(search_url=url)
+url = "https://iwf.sport/results/results-by-events/?event_id=535"
+result = client.get_results(event_url=url)
 print(result)
