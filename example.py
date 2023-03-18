3a17e216a5b20bc949585303ca464b520a818eba
jwc20 <choij2015@gmail.com>
2023-03-17 20:11:15 -0700

    Add another example url.

--- example.py
+++ example.py
@@ -6,6 +6,7 @@ client = iwf.Iwf()
 #   print(event)
 #   print(event['name'])
 
-url = "https://iwf.sport/results/results-by-events/?event_id=535"
+# url = "https://iwf.sport/results/results-by-events/?event_id=535"
+url = "https://iwf.sport/results/results-by-events/results-by-events-old-bw/?event_id=58"
 result = client.get_results(event_url=url)
 print(result)
