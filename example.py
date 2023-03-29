145d54df467d40c032b6efa0cac7d40b0422984a
jwc20 <choij2015@gmail.com>
2023-03-29 01:23:54 -0700

    Update

--- example.py
+++ example.py
@@ -1,12 +1,17 @@
+from pprintpp import pprint
 import iwf
 
 client = iwf.Iwf()
 
-for event in client.get_events():
-  print(event)
-  # print(event['name'])
+# for event in client.get_events():
+#   print(event)
+# print(event['name'])
 
 # url = "https://iwf.sport/results/results-by-events/?event_id=535"
-# url = "https://iwf.sport/results/results-by-events/results-by-events-old-bw/?event_id=58"
-# result = client.get_results(event_url=url)
-# print(result)
+url = "https://iwf.sport/results/results-by-events/results-by-events-old-bw/?event_id=4"
+result = client.get_results(event_url=url)
+pprint(result)
+
+
+# with open("result.txt", "w") as f: 
+#     f.write(str(result))
